9abbcfd84b791f0615199bf1c79ff7564077549d
Thuan Dang <thuandang@MacM5.local>
2026-04-16 20:14:32 -0500

    fix dev

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+import nextra from 'nextra'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withNextra = nextra({
+  contentDirBasePath: '/'
+})
 
-export default nextConfig;
+export default withNextra({})
